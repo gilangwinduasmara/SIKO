49f288cd5d1a1a5924bb217704b9017c618d1d50
gilangwinduasmara <gilang_windu_asmara@yahoo.com>
2021-01-29 17:42:28 +0700

    menu icons

--- config/menu_aside.php
+++ config/menu_aside.php
@@ -7,28 +7,28 @@ return [
             [
                 'title' => 'Dashboard',
                 'root' => true,
-                'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                'icon' => 'flaticon-home-2', // or can be 'flaticon-home' or any flaticon-*
                 'page' => '/dashboard',
                 'new-tab' => false,
             ],
             [
                 'title' => 'Daftar Konseli',
                 'root' => true,
-                'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                'icon' => 'flaticon-users', // or can be 'flaticon-home' or any flaticon-*
                 'page' => '/daftarkonseli',
                 'new-tab' => false,
             ],
             [
                 'title' => 'Case Conference',
                 'root' => true,
-                'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                'icon' => 'flaticon2-group', // or can be 'flaticon-home' or any flaticon-*
                 'page' => '/caseconference',
                 'new-tab' => false,
             ],
             [
                 'title' => 'Arsip',
                 'root' => true,
-                'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                'icon' => 'flaticon-folder-1', // or can be 'flaticon-home' or any flaticon-*
                 'page' => '/arsip',
                 'new-tab' => false,
             ]
@@ -37,21 +37,21 @@ return [
                 [
                     'title' => 'Dashboard',
                     'root' => true,
-                    'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                    'icon' => 'flaticon-home-2', // or can be 'flaticon-home' or any flaticon-*
                     'page' => '/dashboard',
                     'new-tab' => false,
                 ],
                 [
                     'title' => 'Arsip',
                     'root' => true,
-                    'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                    'icon' => 'flaticon-folder-1', // or can be 'flaticon-home' or any flaticon-*
                     'page' => '/arsip',
                     'new-tab' => false,
                 ]
             ],
             'admin' => [
                 [
-                    'title' => 'Dashboard',
+                    'title' => 'flaticon-analytics',
                     'root' => true,
                     'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
                     'page' => '/admin/dashboard',
@@ -60,25 +60,25 @@ return [
                 [
                     'title' => 'Kelola Konselor',
                     'root' => true,
-                    'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                    'icon' => 'flaticon-user-settings', // or can be 'flaticon-home' or any flaticon-*
                     'page' => '/admin/konselor',
                     'new-tab' => false,
                 ],
                 [
                     'title' => 'Report',
-                    'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                    'icon' => 'flaticon2-poll-symbol', // or can be 'flaticon-home' or any flaticon-*
                     'submenu' =>[
                             [
                                 'title' => 'Presensi',
                                 'root' => true,
-                                'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                                'icon' => 'flaticon-list-3', // or can be 'flaticon-home' or any flaticon-*
                                 'page' => '/admin/report',
                                 'new-tab' => false,
                             ],
                             [
                                 'title' => 'Detail Konseling',
                                 'root' => true,
-                                'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                                'icon' => 'flaticon-list', // or can be 'flaticon-home' or any flaticon-*
                                 'page' => '/admin/report?detail=true',
                                 'new-tab' => false,
                             ]
@@ -86,19 +86,19 @@ return [
                 ],
                 [
                     'title' => 'Setting',
-                    'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                    'icon' => 'flaticon-cogwheel-2', // or can be 'flaticon-home' or any flaticon-*
                     'submenu' =>[
                             [
                                 'title' => 'Expired Date',
                                 'root' => true,
-                                'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                                'icon' => 'flaticon-calendar-with-a-clock-time-tools', // or can be 'flaticon-home' or any flaticon-*
                                 'page' => '/admin/setting?submenu=expiration',
                                 'new-tab' => false,
                             ],
                             [
                                 'title' => 'Maksimum Konseli',
                                 'root' => true,
-                                'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                                'icon' => 'flaticon-warning', // or can be 'flaticon-home' or any flaticon-*
                                 'page' => '/admin/setting?submenu=maxkonseli',
                                 'new-tab' => false,
                             ]
@@ -106,12 +106,12 @@ return [
                 ],
                 [
                     'title' => 'Informasi Pengumuman',
-                    'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                    'icon' => 'flaticon-notes', // or can be 'flaticon-home' or any flaticon-*
                     'submenu' =>[
                             [
                                 'title' => 'Pengumuman',
                                 'root' => true,
-                                'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                                'icon' => 'flaticon-exclamation-2', // or can be 'flaticon-home' or any flaticon-*
                                 'page' => '/admin/informasi?submenu=pengumuman',
                                 'new-tab' => false,
                             ],
@@ -119,7 +119,7 @@ return [
                             [
                                 'title' => 'Quote',
                                 'root' => true,
-                                'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+                                'icon' => 'flaticon-speech-bubble-1', // or can be 'flaticon-home' or any flaticon-*
                                 'page' => '/admin/informasi?submenu=quote',
                                 'new-tab' => false,
                             ]
